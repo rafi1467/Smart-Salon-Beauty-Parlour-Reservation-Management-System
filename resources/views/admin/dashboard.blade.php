25bc2e2348aab5a5c524c5b8bcd61d73f4f6418f
gh0st1457 <sami2201501457@diu.edu.bd>
2025-11-17 23:19:05 +0600

    dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+<h1 class="text-primary">Admin Dashboard</h1>
+
+<div style="display: grid; grid-template-columns: 1fr 1fr; gap: 20px;">
+
+    <div class='card'>
+        <h3 class='text-main'>Appointments</h3>
+        <p>Bookings Today: <strong class='text-primary'>todaysBookings</strong></p>
+        <p>Upcoming Confrimed: <strong class='test-primary'>upcomingBookings</strong></p>
