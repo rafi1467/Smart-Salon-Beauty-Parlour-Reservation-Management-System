185e63e73fd987563ef5f67726db213ef79604d2
rafi1467 <rafi22205101467@diu.edu.bd>
2025-11-18 22:59:24 +0600

    Homepage

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
 {
-    //
+    public function home()
+    {
+        $featuredServices = Service::available()->take(4)->get();
+        return view('home', compact('featuredServices'));
+    }
+
+    public function index()
+    {
+        $services = Service::available()->get();
+        return view('services.index', compact('services'));
+    }
+
+    public function show(Service $service)
+    {
+        $reviews = $service->reviews()->approved()->with('user')->get();
+        return view('services.show', compact('service', 'reviews'));
+    }
 }
