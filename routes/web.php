b4083b0f1d325f64396d8206a52f1f673b187a5e
AnikKumarKuri <kuri22205101077@diu.edu.bd>
2025-11-20 19:59:30 +0600

    checking booking  history with web

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
-// Home: for now just redirect to booking history
+// Home: redirect to booking history
 Route::get('/', function () {
     return redirect()->route('bookings.index');
 });
 
-// Protect these with auth if your team has login set up
-Route::middleware('auth')->group(function () {
-    Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
-    Route::post('/bookings/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
-});
-
-// If you DON'T have auth yet and it gives an error,
-// you can temporarily remove "middleware('auth')" and just keep:
-// Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
-// Route::post('/bookings/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
-
+// NO auth middleware for now
+Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
+Route::post('/bookings/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
