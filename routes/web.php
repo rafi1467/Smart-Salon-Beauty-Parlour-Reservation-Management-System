6d25c608a4adfa5e70c66e991b4fc41119ee5a71
Samiul Hasan Sakib <samiulhasansakib@Samiuls-MacBook-Air.local>
2025-11-21 16:53:44 +0600

    chatbot1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BookingController;
 // Home: redirect to booking history
 Route::get('/', function () {
     return view('pages.home');
@@ -10,3 +10,20 @@ Route::get('/', function () {
 Route::get('/chat-bot', function () {
     return view('pages.chatbot');
 });
+
+Route::get('/', function () {
+    return redirect()->route('bookings.index');
+});
+
+// Booking routes
+Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
+
+// Booking confirmation / details
+Route::get('/bookings/{booking}', [BookingController::class, 'show'])->name('bookings.show');
+
+// Booking invoice
+Route::get('/bookings/{booking}/invoice', [BookingController::class, 'invoice'])->name('bookings.invoice');
+
+// Cancel booking
+Route::post('/bookings/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
+
