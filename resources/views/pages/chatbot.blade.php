c54dbd9370923ad62d09ea141cc75221d9c60680
Samiul1902 <sakib22205101472@diu.edu.bd>
2025-11-27 10:57:34 +0600

    examp

--- resources/views/pages/chatbot.blade.php
+++ resources/views/pages/chatbot.blade.php
@@ -3,10 +3,12 @@
 <link rel="stylesheet" href="/css/chat_bot_styles.css">
 
 <link rel="icon" type="image/png" href="/images/WhatsApp_Image_2025-11-17_at_00.14.30-removebg-preview.png"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 <!-- main body -->
 
 
 <body>
+    @include('partials.header')
     <div class="chat-container">
         <header>
             <img src="images/Untitled design.png" alt="no image" class="robot-image" height="50" width="50">
