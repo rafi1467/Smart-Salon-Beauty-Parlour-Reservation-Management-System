c27dda2a1540fa312ec1cdcd9c2bdba0dddf91f1
gh0st1457 <sami22205101457@diu.edu.bd>
2025-11-29 13:07:20 +0600

    oh my god

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,13 +10,13 @@
         <div class="card" style="border-left: 5px solid #5D8AA8;">
             <h3 class="text-main">Appointments</h3>
             <p>Today: <strong class="text-primary" style="font-size: 1.2em;">{{ $todaysBookings }}</strong></p>
-            <p>Upcoming: <strong class="text-primary">{{ $upcomingBookings }}</strong></p>
+            <p>Upcoming: </p>
         </div>
 
         <div class="card" style="border-left: 5px solid #88B04B;">
             <h3 class="text-main">Sales Revenue</h3>
-            <p>Today: <strong class="text-highlight" style="font-size: 1.2em;">৳ {{ number_format($todaysSales, 2) }}</strong></p>
-            <p>This Month: <strong class="text-highlight">৳ {{ number_format($monthlySales, 2) }}</strong></p>
+            <p>Today: </p>
+            <p>This Month: </p>
         </div>
 
         <div class="card" style="border-left: 5px solid #E2725B;">
