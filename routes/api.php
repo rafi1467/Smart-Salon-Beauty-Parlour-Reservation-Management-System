560a22ebeefb65dda77051888978dd0c9f39b539
Samiul1902 <sakib22205101472@diu.edu.bd>
2025-12-11 16:58:40 +0600

    feat: Add new `auth.php` route file and adjust Symfony package versions in `composer.lock`.

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AppointmentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::post('/appointment/book', [AppointmentController::class,'book']);
-Route::post('/appointment/cancel/{id}', [AppointmentController::class,'cancel']);
-Route::post('/appointment/reschedule/{id}', [AppointmentController::class,'reschedule']);
