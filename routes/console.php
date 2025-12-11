bda80ae0fb63563600f12cabe882054830fa4aae
Samiul1902 <sakib22205101472@diu.edu.bd>
2025-12-11 20:51:08 +0600

    chore: delete API route file and update console reminder schedule frequency.

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use App\Console\Commands\SendReminders;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(SendReminders::class)->everyFiveMinutes();
+\Illuminate\Support\Facades\Schedule::command('appointments:remind')->hourly();
